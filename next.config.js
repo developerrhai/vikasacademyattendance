430ea03414852f974e74f7435b027fc9a7f93b03
nishantitsaoirse-head <nishant.it.saoirse@gmail.com>
2026-07-14 14:36:16 +0530

    chore: remove swcMinify warning from next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: false,
   turbopack: {
     root: path.join(__dirname),
   },
